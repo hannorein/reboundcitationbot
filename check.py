8592eecfd9b419e06cce2a4e5ba1aa73f00e3537
Hanno Rein <hanno@hanno-rein.de>
2020-10-19 11:46:03 -0400

    added author names

--- check.py
+++ check.py
@@ -14,7 +14,7 @@ with open("adskey.txt") as f:
 headers = {"Authorization": "Bearer "+token}
 bibcodestocheck = ["2015MNRAS.446.1424R", "2012A&A...537A.128R", "2015MNRAS.452..376R", "2018MNRAS.473.3351R", "2019MNRAS.485.5490R", "2011MNRAS.415.3168R", "2011ascl.soft10016R", "2020MNRAS.491.2885T"]
 q = "citations(bibcode:"+(") or citations(bibcode:".join(bibcodestocheck))+")"
-params = {"q":q, "rows":"2000","fl":"bibcode,pub,title"}
+params = {"q":q, "rows":"2000","fl":"bibcode,pub,title,author"}
 url = "https://api.adsabs.harvard.edu/v1/search/bigquery"
 r = requests.get(url, headers=headers, params=params)
 response = r.json()["response"]
@@ -34,11 +34,16 @@ for p in response["docs"]:
     bibcode = p["bibcode"]
     if bibcode not in oldc or bibcode == debug:
         if not firstrun:
-            debug = False
             pub = p["pub"]
             title = p["title"][0]
+            authors = p["author"]
+            authortxt = authors[0].split(",")[0]
+            if len(authors)==2:
+                authortxt += " & "+authors[1].split(",")[0]
+            if len(authors)>2:
+                authortxt += " et al."
             maxlength = 280 - 25
-            text = "A new paper has cited REBOUND:\n"+title
+            text = "A new paper by "+authortxt+" has cited REBOUND:\n"+title
             if len(text)>maxlength:
                 text = text[:maxlength-2] + '..' 
             url = "https://ui.adsabs.harvard.edu/abs/"+bibcode+"/abstract"
